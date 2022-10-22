6674ef87dc5cf6003699d8140236a76dd293c16e
Angela <chepkoechangela514@gmail.com>
2022-10-22 10:47:05 +0300

    done

--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -1,54 +1,35 @@
-require_relative 'spec_helper'
-require_relative '../methods'
-
-describe '#greet_programmer' do
-
-  it 'outputs the string "Hello, programmer!"' do
-    expect { greet_programmer }.to output(a_string_including("Hello, programmer!")).to_stdout
-  end
-  
+# Your code here!
+#greet programmer
+def greet_programmer
+  puts "Hello, programmer!"
 end
+greet_programmer
 
-describe '#greet' do
-
-  it 'outputs a string "Hello, Naureen!" when called with "Naureen"' do
-    expect { greet("Naureen") }.to output(a_string_including("Hello, Naureen!")).to_stdout
-  end
-
-  it 'outputs a string "Hello, Jimmy!" when called with "Jimmy"' do
-    expect { greet("Jimmy") }.to output(a_string_including("Hello, Jimmy!")).to_stdout
-  end
-
+#greet Maureen
+def greet(name = "Maureen")
+  puts "Hello, #{name}!"
 end
+greet
 
-describe '#greet_with_default' do
-
-  it 'outputs a string "Hello, Naureen!" when called with "Naureen"' do
-    expect { greet_with_default("Naureen") }.to output(a_string_including("Hello, Naureen!")).to_stdout
-  end
-
-  it 'outputs a string "Hello, programmer!" when called with no arguments' do
-    expect { greet_with_default }.to output(a_string_including("Hello, programmer!")).to_stdout
-  end
-
+#greet with default
+def greet_with_default(name = "Naureen")
+  puts "Hello, #{name}!"
+  puts "Hello, programmer!"
 end
+   greet_with_default
 
-describe '#add' do
-
-  it 'returns the sum of two numbers' do
-    expect(add(2, 5)).to be(7)
-  end
-
+#returns the sum of two numnbers
+def add(num1, num2)
+ return num1 + num2
 end
+add(2, 5)
 
-describe '#halve' do
-  
-  it 'returns half of the given integer' do
-    expect(halve(6)).to be(3)
-  end
-
-  it 'returns nil if not given a integer' do
-    expect(halve("six")).to be(nil)
-  end
-
+#returns half of the given integer
+def halve(num)
+   return num / 2
 end
+def halve(num)
+   return num / 2 if num.is_a? Integer
+   nil
+end
+halve(6)
